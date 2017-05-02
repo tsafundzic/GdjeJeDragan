4f2ae041899606c1ac596bb3ccb30ff74f14338a
Tomislav <tsafundzic@gmail.com>
2017-05-02 17:58:48 +0200

    Added map and simple marker onclick

--- app/src/main/java/com/example/tomislav/gdjejedragan/MainActivity.java
+++ app/src/main/java/com/example/tomislav/gdjejedragan/MainActivity.java
@@ -1,13 +1,133 @@
 package com.example.tomislav.gdjejedragan;
 
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.Manifest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements OnMapReadyCallback {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 10;
+    GoogleMap mGoogleMap;
+    MapFragment mMapFragment;
+    private GoogleMap.OnMapClickListener mCustomOnMapClickListener;
+    private TextView tvLocationDisplay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setUI();
     }
+
+    private void setUI() {
+        this.tvLocationDisplay = (TextView) this.findViewById(R.id.tvLocationDisplay);
+
+        this.mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fGoogleMap);
+        this.mMapFragment.getMapAsync(this);
+        this.mCustomOnMapClickListener = new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions newMarkerOptions = new MarkerOptions();
+                newMarkerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+                newMarkerOptions.title("My place");
+                newMarkerOptions.snippet("I declare this my teritory!");
+                newMarkerOptions.position(latLng);
+                mGoogleMap.addMarker(newMarkerOptions);
+            }
+        };
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mGoogleMap = googleMap;
+        UiSettings uiSettings = this.mGoogleMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setZoomGesturesEnabled(true);
+        this.mGoogleMap.setOnMapClickListener(this.mCustomOnMapClickListener);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling ActivityCompat#requestPermissions
+            return;
+        }
+        this.mGoogleMap.setMyLocationEnabled(true);
+    }
+
+    private boolean hasLocationPermission(){
+        String LocationPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+        int status = ContextCompat.checkSelfPermission(this,LocationPermission);
+        if(status == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        return false;
+    }
+    private void requestPermission(){
+        String[] permissions = new String[]{ Manifest.permission.ACCESS_FINE_LOCATION };
+        ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_LOCATION_PERMISSION);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Log.d("Permission", "Permission granted. User pressed allow.");
+                    } else {
+                        Log.d("Permission", "Permission not granted. User pressed deny.");
+                        askForPermission();
+                    }
+                }
+        }
+    }
+
+    private void askForPermission(){
+        boolean shouldExplain = ActivityCompat.shouldShowRequestPermissionRationale(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if(shouldExplain){
+            Log.d("Permission","Permission should be explained, - don't show again not clicked.");
+            this.displayDialog();
+        }
+        else{
+            Log.d("Permission","Permission not granted. User pressed deny and don't show again.");
+            tvLocationDisplay.setText("Sorry, we really need that permission");
+        }
+    }
+    private void displayDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Location permission")
+                .setMessage("We display your location and need your permission")
+                .setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("Permission", "User declined and won't be asked again.");
+                        dialog.cancel();
+                    }
+                })
+                .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("Permission","Permission requested because of the explanation.");
+                        requestPermission();
+                        dialog.cancel();
+                    }
+                })
+                .show();
+}
 }
